a68c048696fcef467cb356da703d77282cf3d0be
pradyun-coder <71138767+pradyun-coder@users.noreply.github.com>
2025-04-14 18:20:20 +0530

    Update thirteenthproj.py

--- thirteenthproj.py
+++ thirteenthproj.py
@@ -1,14 +1,15 @@
 #gcd finder
 number6 = input("Enter a number")
 number5 = input("Enter another number")
-factors = []
-nofactors = 0
+
 if number6<=number5:
     maxcount = int(number6)
+    mincount = int(number5)
 else:
     maxcount = int(number5)
-for i in range (1, maxcount):
-    if int(number6)%int(i) == 0 and int(number5)%int(i) == 0:
-        factors.append(i)
-        nofactors = nofactors +1
-print(factors[nofactors-1])
+    mincount = int(number6)
+    
+while maxcount%mincount != 0:
+    maxcount = maxcount + mincount
+
+print(maxcount)
