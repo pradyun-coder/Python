5bff4a17814c49f75efdc0787bfc320f8553bd67
pradyun-coder <71138767+pradyun-coder@users.noreply.github.com>
2025-04-14 18:34:44 +0530

    Update thirteenthproj.py

--- thirteenthproj.py
+++ thirteenthproj.py
@@ -1,17 +1,17 @@
-#gcd finder
+#lcm finder
 number6 = input("Enter a number")
 number5 = input("Enter another number")
 
 if number6<=number5:
-    maxcount = int(number6)
-    mincount = int(number5)
-else:
-    maxcount = int(number5)
     mincount = int(number6)
+    maxcount = int(number5)
+else:
+    mincount = int(number5)
+    maxcount = int(number6)
     
 while maxcount%mincount != 0:
-    maxcount = maxcount + 1
     if maxcount%mincount == 0:
         break
+    maxcount = maxcount + 1
 
-print(maxcount + 1)
+print(maxcount)
