0c5d0de3cc4f7e94610b8258c23361dfbd2a8b2b
pradyun-coder <71138767+pradyun-coder@users.noreply.github.com>
2025-04-28 18:35:20 +0530

    Add files via upload

---
+++ seventeenthproj.py
@@ -0,0 +1,13 @@
+def seive(num):
+    primes = [True for i  in range(num +1)]
+    p = 2
+    while p*p <= num:
+        if primes[p] == True:
+            for i in range(p*p, num +1, p):
+                primes[i] = False
+        p += 1
+    for p in range(2, num +1):
+        if primes[p]:
+            print(p)
+a = 100
+seive(a)
